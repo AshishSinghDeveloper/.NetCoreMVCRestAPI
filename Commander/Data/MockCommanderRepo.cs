54f5e928d92bc9236a03c56eddaa09961569841a
AshishSinghDeveloper <ashishsng21@hotmail.com>
2020-10-06 18:05:09 -0500

    Creates mock repo to implement interface

---
+++ Commander/Data/MockCommanderRepo.cs
@@ -0,0 +1,24 @@
+using System.Collections.Generic;
+using Commander.Models;
+
+namespace Commander.Data
+{
+    public class MockCommanderRepo : ICommanderRepo
+    {
+        public IEnumerable<Command> GetAppCommands()
+        {
+            var commands = new List<Command>
+            {
+                new Command{Id=1, HowTo="Prepare a cup of Tea", Line="Place Kettle in Stove", Platform="Kettle and Cup"},
+                new Command{Id=2, HowTo="Prepare a glass of Juice", Line="Get Carrots", Platform="Juicer and glass"},
+                new Command{Id=3, HowTo="Prepare a plate of Pakoda", Line="Cut Onions and chili", Platform="Pan and stove"},
+            };
+            return commands;
+        }
+
+        public Command GetCommandById(int id)
+        {
+            return new Command{Id=0, HowTo="Boil an egg", Line="Boil Water", Platform="Kettle & Pan"}
+        }
+    } 
+}
